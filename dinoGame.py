908c384cbecdfecc7aba06af5c9efd9eaab062a8
110213018 <148731321+110213018@users.noreply.github.com>
2024-01-03 23:03:06 +0800

    Update dinoGame.py

--- dinoGame.py
+++ dinoGame.py
@@ -1,5 +1,8 @@
 from ursina import *
+import time
 import random as r
+
+timer = 0
 
 app = Ursina()
 window.fullscreen = True
@@ -31,6 +34,7 @@ cacti = []
 def newCactus():
   new = duplicate(cactus,
                   x=12+r.randint(0,5))
+  new.has_been_scored = False
   cacti.append(new)
   invoke(newCactus, delay=2)
 
@@ -43,22 +47,38 @@ label = Text(
   color=color.black,
   position=(-0.5, 0.4)
 )
+timeLabel = Text(
+  text = f'Time: {0}',
+  color=color.black,
+  position=(-0.5, 0.35)
+)
 points = 0
+game_duration = 30  # Set the game duration to 10 seconds
 
 def update():
   global points
-  points += 1
-  label.text = f'Points: {points}'
-  for ground in pair:
-    ground.x -= 6*time.dt
-    if ground.x < -35:
-      ground.x += 100
-  for c in cacti:
-    c.x -= 6*time.dt
-  if dino.intersects().hit:
-    dino.texture= 'assets\hit'
+  global timer
+  if timer < game_duration:
+    timer += time.dt
+    timeLabel.text = f'Time: {int(timer)}'
+    for ground in pair:
+      ground.x -= 6*time.dt
+      if ground.x < -35:
+        ground.x += 100
+    for c in cacti:
+      c.x -= 6*time.dt
+      if c.x < dino.x and not c.has_been_scored:
+        c.has_been_scored = True
+        points += 1
+        label.text = f'Points: {points}'
+    if dino.intersects().hit:
+      dino.texture= 'assets\hit'
+      application.pause()
+      points -= 1
+      time.sleep(1)
+      application.resume()
+  else:
     application.pause()
-
 
 sound = Audio(
   'assets\\beep',
@@ -68,7 +88,7 @@ sound = Audio(
 
 def input(key):
   print(f'Key pressed: {key}')
-  if key == 'space up':
+  if key == 'enter':
     print('Jumping...')
     if dino.y < 0.01:
       sound.play()
@@ -83,8 +103,5 @@ def input(key):
         delay=0.4,
         curve = curve.in_sine
       )
-
-camera.orthographic = True
-camera.fov = 10
 
 app.run()
